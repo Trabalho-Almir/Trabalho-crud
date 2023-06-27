6a587b2ba6d5d7214bf4e7674a59a9343df90752
Kainanramos <105524576+Kainanramos@users.noreply.github.com>
2023-06-27 15:53:44 -0300

    continuação crud

---
+++ js/monitor.js
@@ -0,0 +1,100 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sEstoque = document.querySelector('#m-estoque')
+const sPreco = document.querySelector('#m-preco')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+  modal.classList.add('active')
+
+  modal.onclick = e => {
+    if (e.target.className.indexOf('modal-container') !== -1) {
+      modal.classList.remove('active')
+    }
+  }
+
+  if (edit) {
+    sEstoque.value = itens[index].estoque
+    sNome.value = itens[index].nome
+    sPreco.value = itens[index].preco
+    id = index
+  } else {
+    sEstoque.value = ''
+    sNome.value = ''
+    sPreco.value = ''
+  }
+  
+}
+
+function editItem(index) {
+
+  openModal(true, index)
+}
+
+function deleteItem(index) {
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td>${item.nome}</td>
+    <td>${item.estoque}</td>
+    <td>R$ ${item.preco}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+  tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+  
+  if (sNome.value == '' || sEstoque.value == '' || sPreco.value == '') {
+    return
+  }
+
+  e.preventDefault();
+
+  if (id !== undefined) {
+    itens[id].nome = sNome.value
+    itens[id].estoque = sEstoque.value
+    itens[id].preco = sPreco.value
+  } else {
+    itens.push({'nome': sNome.value,'estoque': sEstoque.value , 'preco': sPreco.value})
+  }
+
+  setItensBD()
+
+  modal.classList.remove('active')
+  loadItens()
+  id = undefined
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+
+}
+
+//slas teste
+
+
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbmonitor')) ?? []
+const setItensBD = () => localStorage.setItem('dbmonitor', JSON.stringify(itens))
+
+loadItens()
